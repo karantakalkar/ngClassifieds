466cc7178e4b814fe1468643ae3c1acb647ffc0c
karan <karant@iitk.ac.in>
2020-03-02 11:54:20 +0530

    feat: configure  index.html and app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-angular.module("ngClassifieds", []);
+(function() {
+
+    "use strict";
+  
+    angular
+      .module('ngClassifieds', ['ngMaterial'])
+      .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+          .primaryPalette('teal')
+          .accentPalette('orange');
+      });
+          
+  })();
